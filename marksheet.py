75bd6f91c01aa8e68c30e73ac22aaa50f795106e
shahid786ali <shahidalizaddi@gmail.com>
2022-05-30 07:43:49 -0700

    this is simple marksheet making program

---
+++ marksheet.py
@@ -0,0 +1,48 @@
+# # math, science,urdu,p_study=input("Please input your subject math,sicence,urdu,p_study marks then we create your markssheet");
+# math= int(input("please input your math marks ="))
+# science =int(input("Please input your science marks ="))
+# urdu=int(input("please input your urdu marks ="))
+# p_study=int(input("please input your pak study marks="))
+# # math=84;
+# # science=95;
+# # urdu=45;
+# # p_study=33;
+# total=100;
+# m_t=int(math/total * 100); u_t=int(urdu/total *100);p_t=int(p_study/total * 100 ); s_t=int(science/total *100); t_p=(m_t+u_t+p_t+s_t)
+# t_m=(math+science+urdu+p_study);
+# t_p=(math+science+urdu+p_study)/400 *100;
+# if math<33:
+#     m="fail"
+# else:
+#     m="pass"
+# if science<33:
+#     s="fail"
+# else:
+#     s="pass"
+# if urdu<33:
+#     u="fail"
+# else:
+#     u="pass"
+# if p_study<33:
+#     p="fail"
+# else:
+#     p="pass"
+
+# print("\ln \ln")
+
+
+# print("###############################################################################################################")
+# print("                                              MARK SHEET                                                       ") 
+# print("   subject        obt.marks     T.marks         status        percentage       total O.marks                     ") 
+# print("   Math             ",math ,   "    100                 ",m,"         ",  m_t,"%" )
+# print("   science          ",science, "    100                 ",s,"         ",  s_t,"%")
+# print("   urdu             ",urdu ,   "    100                 ",u,"         ",  u_t,"%")
+# print("   P.study          ",p_study ,"    100                 ",p,"         ",  p_t,"%")
+# print("_______________________________________________________________________________________________________________")
+# print("                                                                 ",t_p,"%",("       "), t_m)
+
+# print("This candidate passed the exma in the marks ", t_m,("out of "), 400 )
+
+
+
+# print("###############################################################################################################")
